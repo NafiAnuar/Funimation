dd42b246c532904e2d4ff8dd644e934a1bff95f8
NafiAnuar <Nafianuar@gmail.com>
2018-03-13 15:38:41 +0800

    asasasa

--- Funimation/Login.aspx.cs
+++ Funimation/Login.aspx.cs
@@ -30,7 +30,7 @@ namespace Funimation
             }
             else
             {
-                litLoginError.Text = "Invalid username or passwords";
+                litLoginError.Text = "Invalid username or passwordz";
             }
         }
 
